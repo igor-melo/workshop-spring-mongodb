ed570f10aa93be5a2fc0f5efc296b7b3e63d9138
Igor Melo <i90rmc@mail.com>
2021-12-05 17:52:54 -0300

    criado resource e service, conexão com mongodb

--- src/main/java/com/nelio/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/nelio/workshopmongodb/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.nelio.workshopmongodb.resources;
 
 import com.nelio.workshopmongodb.domain.User;
+import com.nelio.workshopmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResources {
+public class UserResource {
+
+
+    @Autowired
+    private UserService service;
 
     @GetMapping
     //ResponseEntity encapsula para retorno http
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "maria", "maria@gmail.com");
-        User alex = new User("2", "alex", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+//        User maria = new User("1", "maria", "maria@gmail.com");
+//        User alex = new User("2", "alex", "alex@gmail.com");
+
+        List<User> list = service.findAll();
+//        list.addAll(Arrays.asList(maria, alex));
         return ResponseEntity.ok().body(list);
     }
 }
