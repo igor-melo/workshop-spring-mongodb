e61069611e513bc28c0504a64021b8e771750a89
Igor Melo <i90rmc@mail.com>
2021-12-08 17:00:46 -0300

    obtendo post por commit

---
+++ src/main/java/com/nelio/workshopmongodb/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.nelio.workshopmongodb.resources;
+
+import com.nelio.workshopmongodb.domain.Post;
+import com.nelio.workshopmongodb.domain.User;
+import com.nelio.workshopmongodb.dto.UserDTO;
+import com.nelio.workshopmongodb.services.PostService;
+import com.nelio.workshopmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/nelio/workshopmongodb/services/PostService.java
@@ -0,0 +1,34 @@
+package com.nelio.workshopmongodb.services;
+
+import com.nelio.workshopmongodb.domain.Post;
+import com.nelio.workshopmongodb.domain.User;
+import com.nelio.workshopmongodb.dto.UserDTO;
+import com.nelio.workshopmongodb.repository.PostRepository;
+import com.nelio.workshopmongodb.repository.UserRepository;
+import com.nelio.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id){
+        Optional<Post> post = repo.findById(id);
+        if (post.isEmpty()){
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+
+        Post userEntity = post.get();
+
+        return userEntity;
+    }
+
+
+}
