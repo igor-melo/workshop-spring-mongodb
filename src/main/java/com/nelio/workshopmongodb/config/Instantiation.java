40ad1aa02fc834237ddd51bda3124031483392be
Igor Melo <i90rmc@mail.com>
2021-12-08 16:09:44 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/nelio/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/nelio/workshopmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.nelio.workshopmongodb.config;
 
+import com.nelio.workshopmongodb.domain.Post;
 import com.nelio.workshopmongodb.domain.User;
+import com.nelio.workshopmongodb.repository.PostRepository;
 import com.nelio.workshopmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,10 +18,17 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User joao = new User(null, "João", "joao@mail.com");
         User maria = new User(null, "Maria", "maria@mail.com");
@@ -28,7 +39,14 @@ public class Instantiation implements CommandLineRunner {
         User sophia = new User(null, "sophia", "Sophia@mail.com");
         User isabella = new User(null, "isabella", "Isabella@mail.com");
 
+        Post post1 = new Post(null, sdf.parse("01/03/2018"), "Partiu viagem!", "Vou viajar para SP abraços!", joao);
+        Post post2 = new Post(null, sdf.parse("15/03/2018"), "Dicas rápidas", "Beba agua", maria);
+        Post post3 = new Post(null, sdf.parse("06/04/2018"), "Sites gratuitos", "Google", helena);
+        Post post4 = new Post(null, sdf.parse("18/06/2018"), "Dicas para melhorar ", "Se ajude", alice);
+        Post post5 = new Post(null, sdf.parse("11/07/2018"), "O segredo", "Nao conto", joao);
+
         userRepository.saveAll(Arrays.asList(joao, maria ,helena,alice, laura, manuela, sophia, isabella ));
+        postRepository.saveAll(Arrays.asList(post1,post2,post3,post4,post5));
 
     }
 }
---
+++ src/main/java/com/nelio/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.nelio.workshopmongodb.repository;
+
+import com.nelio.workshopmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
